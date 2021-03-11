6948cccb251e06ab228e5caae4510d6954483e17
Борис <surgeon12111@yandex.ru>
2021-03-11 15:27:01 +0300

    Version after 2nd review

--- posts/forms.py
+++ posts/forms.py
@@ -1,5 +1,4 @@
 from django.forms import ModelForm
-from django.utils.translation import gettext_lazy as gtl
 
 from .models import Post
 
@@ -9,16 +8,16 @@ class PostForm(ModelForm):
         model = Post
         fields = ['text', 'group']
         labels = {
-            'text': gtl('Текст Вашего поста'),
+            'text': 'Текст Вашего поста',
         }
         help_texts = {
-            'text': gtl('Здесь Вы можете рассказать, что у Вас нового.'),
-            'group': gtl('Выберите группу, которая лучше всего '
-                         'подходит к теме Вашего поста.')
+            'text': 'Здесь Вы можете рассказать, что у Вас нового.',
+            'group': 'Выберите группу, которая лучше всего '
+                     'подходит к теме Вашего поста.'
         }
         error_messages = {
             'text': {
-                'required': gtl('Посты без текста мало кому интересны, '
-                                'пожалуйста, поделитесь своей историей!'),
+                'required': 'Посты без текста мало кому интересны, '
+                            'пожалуйста, поделитесь своей историей!',
             },
         }
